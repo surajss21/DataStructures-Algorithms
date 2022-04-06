5ae577a8b0b204d81c7ca62c087ca5657f46da33
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 14:35:14 +0530

    Update Min_Max_Array_Recur.py

--- Arrays/2_Max_Min_Element_in_Array/Min_Max_Array_Recur.py
+++ Arrays/2_Max_Min_Element_in_Array/Min_Max_Array_Recur.py
@@ -1,1 +1,32 @@
+# T.C O(N)
+def getMinMax(arr, low, high):
+  
+  amin = 0
+  amax = 0
 
+  # Base Case.
+  if(low == high):
+    amin = arr[low]
+    amax = arr[low]
+    return (amin , amax)
+  
+  elif(high == low + 1):
+    if(arr[low] > arr[high]):
+      amax = arr[low]
+      amin = arr[high]
+    else:
+      amin = arr[low]
+      amax = arr[high]
+    return (amin, amax)
+
+  else:
+    mid = (low + high)//2
+    amin1, amax1 = getMinMax(arr, low, mid)
+    amin2, amax2 = getMinMax(arr, mid+1, high)
+    
+  return (min(amin1, amin2), max(amax1, amax2))
+
+arr = [1,2,3,100,5,6,-7]
+minn, maxx = getMinMax(arr, 0, len(arr)-1)
+print(minn)
+print(maxx)
