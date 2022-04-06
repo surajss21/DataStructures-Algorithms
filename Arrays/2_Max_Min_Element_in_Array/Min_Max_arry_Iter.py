81ce5f0458f458a09882ab8e2d56d8e08de0526e
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 13:59:27 +0530

    Update Min_Max_arry_Iter.py

--- Arrays/2_Max_Min_Element_in_Array/Min_Max_arry_Iter.py
+++ Arrays/2_Max_Min_Element_in_Array/Min_Max_arry_Iter.py
@@ -1,1 +1,32 @@
+def getMinMax(arr, n):
 
+  # Create minmax object
+  min = 0
+  max = 0
+
+  # For 1 element
+  if(n == 1):
+    min = arr[0]
+    max = arr[0]
+    return (min, max)
+
+  if(arr[0] < arr[1]):
+    min = arr[0]
+    max = arr[1]
+
+  else:
+    min = arr[1]
+    max = arr[0]
+
+  for i in range(2, n):
+    if(arr[i] > max):
+      max = arr[i]
+    elif(arr[i] < min):
+      min = arr[i]
+
+  return (min, max)  
+
+arr = [1,2,3,100,5,6,-7]
+min, max = getMinMax(arr, len(arr))
+print(min)
+print(max)
