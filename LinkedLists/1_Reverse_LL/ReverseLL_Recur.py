fcf4383ba36c90a512ddeb30d3c3cfd777e1229a
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 16:47:32 +0530

    Update ReverseLL_Recur.py

--- LinkedLists/1_Reverse_LL/ReverseLL_Recur.py
+++ LinkedLists/1_Reverse_LL/ReverseLL_Recur.py
@@ -1,1 +1,18 @@
+def reverseLL(head):
+  
+  # Base Case
+  if(head == None or head.next == None):
+    return head
 
+  # R.c 
+  chotaHead = reverseRec(head.next)
+
+  head.next.next = head
+  head.next = None
+
+  return chotaHead
+
+
+display(head)
+head = reverseLL(head)
+display(head)
