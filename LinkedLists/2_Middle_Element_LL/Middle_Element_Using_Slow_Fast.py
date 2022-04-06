5a0f70d492e8b9749b8b3ee19db1d1e50b503c31
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 17:18:09 +0530

    Update Middle_Element_Using_Slow_Fast.py

--- LinkedLists/2_Middle_Element_LL/Middle_Element_Using_Slow_Fast.py
+++ LinkedLists/2_Middle_Element_LL/Middle_Element_Using_Slow_Fast.py
@@ -1,1 +1,13 @@
+def middle(head):
+  if(head == None or head.next == None):
+    return head
 
+  fast = head.next
+  slow = head
+  while(fast != None):    
+    fast = fast.next
+    if(fast != None):
+      fast = fast.next
+    slow = slow.next
+    
+  return slow
