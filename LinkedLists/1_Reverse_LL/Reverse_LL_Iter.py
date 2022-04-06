21fd14d0adaf24850fe5ccc322314cef1349a1e7
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 16:05:12 +0530

    Update Reverse_LL_Iter.py

--- LinkedLists/1_Reverse_LL/Reverse_LL_Iter.py
+++ LinkedLists/1_Reverse_LL/Reverse_LL_Iter.py
@@ -1,1 +1,16 @@
+def reverseLL(head):
 
+  # Empty List
+  if(head == None or head.next == None):
+    return head
+  else:
+    curr = head
+    prev = None
+    forward = Node
+
+    while(curr != None):
+      forward = curr.next
+      curr.next = prev
+      prev = curr
+      curr = forward
+  return prev
