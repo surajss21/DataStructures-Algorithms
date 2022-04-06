8dbb832ba26ef752a22cc45ca74f2f216893dceb
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 17:08:58 +0530

    Update Middle_Element_Using_Length.py

--- LinkedLists/2_Middle_Element_LL/Middle_Element_Using_Length.py
+++ LinkedLists/2_Middle_Element_LL/Middle_Element_Using_Length.py
@@ -1,1 +1,25 @@
+def getLength(head):
+  
+  count = 0
+  temp = head
+  while(temp != None):
+    count += 1
+    temp = temp.next
+    
+  return count
 
+def getMiddle(head):
+
+  len = getLength(head)
+  ans = len//2
+  cnt = 0
+  temp = head
+
+  while(cnt < ans):
+    cnt += 1
+    temp = temp.next   
+
+  return temp
+
+# Temp is a node
+# cnt is index position
