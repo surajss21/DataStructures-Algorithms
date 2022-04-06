dec308a36ff803a4d2c820a1d9dacc11ab7bdc3b
Suraj Singh <97094550+surajss21@users.noreply.github.com>
2022-04-06 12:20:03 +0530

    Update Reverse_Array_Recursion.py

--- Arrays/Reverse Array/Reverse_Array_Recursion.py
+++ Arrays/Reverse Array/Reverse_Array_Recursion.py
@@ -1,1 +1,12 @@
+def reverseArr(arr, s, e):
+  # Base Case:
+  if(s >= e):
+    return   
+  # Swap
+  arr[s], arr[e] = arr[e], arr[s]
+  # R.C
+  reverseArr(arr, s+1, e-1)
 
+arr = [1,2,3,4,5,6,7]
+reverseArr(arr, 0, len(arr)-1)
+print(arr)
